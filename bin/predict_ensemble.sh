8bbc2f5ce5a4c749a1b45375b27729756c7718c1
ololo123321 <vitkadylin@gmail.com>
2023-11-15 09:12:27 +0300

    refactor bin

--- bin/predict_ensemble.sh
+++ bin/predict_ensemble.sh
@@ -3,12 +3,13 @@ export CUDA_VISIBLE_DEVICES="0"
 export OMP_NUM_THREADS="48"
 
 # read ensemble (skip header)
-ensemble_path=/home/vitaly/PycharmProjects/yars/ensemble_best.tsv
-mapfile -s 1 -t ensemble < $ensemble_path
+root_dir=$(cd .. && pwd)
+ensemble_path=$root_dir/ensembles/ensemble_v30.tsv
+mapfile -s 1 -t ensemble < "$ensemble_path"
 
 # setup directories
-models_dir="$(cd .. && pwd)/models"  # models/fold{1,2,3}/model_name/checkpoint-123/pytorch_model.bin
-predictions_dir="$(cd .. && pwd)/predictions"  # predictions/fold{1,2,3}/model_name__step123.npz
+models_dir="$root_dir/models"  # models/fold{1,2,3}/model_name/checkpoint-123/pytorch_model.bin
+predictions_dir="$root_dir/predictions"  # predictions/fold{1,2,3}/model_name__step123.npz
 data_path="/path/to/test.csv"  # path to .csv with tracks and, optionaly, labels
 embeddings_dir="/path/to/track_embeddings"  # directory with .npy files of track embeddings
 blended_output_path="/path/to/y_pred.csv"
@@ -21,10 +22,10 @@ for x in "${ensemble[@]}"; do
   # iterate over splits
   for i in 1 2 3; do
     step="${a[$i]}"
-    output_dir="${predictions_dir}/fold${i}"
-    mkdir -p ${output_dir}
     echo "fold: ${i}; step: ${step}"
-    python "$(cd .. && pwd)/jobs/predict.py" \
+    output_dir="$predictions_dir/fold${i}"
+    mkdir -p "$output_dir"
+    python "$root_dir/jobs/predict.py" \
       ++checkpoint_path="${models_dir}/fold${i}/${name}/checkpoint-${step}/pytorch_model.bin" \
       ++output_path="${output_dir}/${name}__step${step}.npz" \
       ++data_path=$data_path \
@@ -35,8 +36,8 @@ for x in "${ensemble[@]}"; do
 done
 
 # blend predictions
-python "$(cd .. && pwd)/jobs/blend.py" \
+python "$root_dir/jobs/blend.py" \
   --data_path=${data_path} \
-  --config_path=${ensemble_path} \
-  --predictions_dir=${predictions_dir} \
+  --config_path="$ensemble_path" \
+  --predictions_dir="$predictions_dir" \
   --output_path=${blended_output_path}
--- bin/re_train_ensemble.sh
+++ bin/re_train_ensemble.sh
@@ -2,20 +2,21 @@
 export CUDA_VISIBLE_DEVICES="0"
 export OMP_NUM_THREADS="48"
 
-models_dir=$(cd .. && pwd)/models
+root_dir=$(cd .. && pwd)
+models_dir=$root_dir/models
 embeddings_dir=/path/to/track_embeddings
-output_dir=$(cd .. && pwd)/models_v2  # to prevent overriding parent configs
-python_path=/home/vitaly/anaconda3/bin/python
-script_path=$(cd .. && pwd)/jobs/retrain.py
+output_dir=$root_dir/models_v2  # to prevent overriding parent configs
 
+# iterate over models
 for model_dir in "${models_dir}"/fold1/*; do
   name=$(basename "$model_dir")
+  # iterate over splits
   for fold in 1 2 3; do
     echo "model: $name; fold: $fold"
     output_dir_i="${output_dir}/fold$fold/${name}"
-    ${python_path} "${script_path}" \
-      train_data_path="/$(cd .. && pwd)/splits/train_fold$fold.csv" \
-      valid_data_path="/$(cd .. && pwd)/splits/valid_fold$fold.csv" \
+    python "$root_dir/jobs/retrain.py" \
+      train_data_path="$root_dir/splits/train_fold$fold.csv" \
+      valid_data_path="$root_dir/splits/valid_fold$fold.csv" \
       embeddings_dir=$embeddings_dir \
       hydra.run.dir="${output_dir_i}" \
       training_args.output_dir="${output_dir_i}" \
