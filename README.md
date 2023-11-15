5c5c3031a061b31ef7676e295ed4e96602502b0c
ololo123321 <vitkadylin@gmail.com>
2023-11-15 17:34:18 +0300

    reproduction final submit

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 1st place solution in Yandex Cup 2023 - ML RecSys (audio classification).
 
-Re-train whole ensemble (train each model on 3 splits of 10-folds kf):
-```bash
-cd bin
-bash re_train_ensemble.sh
-```
-
 Make final submission (inference of each model + blending):
-```bash
-cd bin
-bash predict_ensemble.sh
-```
+1. `pip install -r requirements.txt`
+2. Download and unpack [embeddings](https://disk.yandex.ru/d/k-tAIJBnWyo7tA)
+3. Download and unpack [models](https://disk.yandex.ru/d/5ohGVa5XDx9GKg). Structure is exactly the same as in `models`, but files `pytorch_model.bin` are in `checkpoint-*` directories.
+4. Modify `models_dir`, `embeddings_dir`, `blended_output_path` (optional, ensemble output), `ensemble_path` (optional, ensemble config path: choose between two files in `ensembles` directory) in `bin/predict_ensemble.sh`
+5. Run inference:
+    ```bash
+    cd bin
+    bash predict_ensemble.sh
+    ```
 
 I have loaded two best submissions:
 * `ensembles/ensemble_v30.tsv` - public score: 0.3095, private score: 0.3128.
 * `ensembles/ensemble_v31.tsv` - public score: 0.3087, private score: 0.3120. Was selected as final.
+
+Re-train whole ensemble (train each model on 3 splits of 10-folds kf):
+```bas
+cd bin
+bash re_train_ensemble.sh
+```
 
 Code for models selection: `src/beam_search.py`
--- bin/predict_ensemble.sh
+++ bin/predict_ensemble.sh
@@ -9,26 +9,25 @@ mapfile -s 1 -t ensemble < "$ensemble_path"
 
 # setup directories
 models_dir="$root_dir/models"  # models/fold{1,2,3}/model_name/checkpoint-123/pytorch_model.bin
-predictions_dir="$root_dir/predictions"  # predictions/fold{1,2,3}/model_name__step123.npz
-data_path="/path/to/test.csv"  # path to .csv with tracks and, optionaly, labels
 embeddings_dir="/path/to/track_embeddings"  # directory with .npy files of track embeddings
-blended_output_path="/path/to/y_pred.csv"
+predictions_dir="$root_dir/predictions"  # predictions/fold{1,2,3}/model_name__step123.npz
+data_path="$root_dir/splits/test.csv"  # path to .csv with tracks and, optionaly, labels
+blended_output_path="$root_dir/predictions/y_pred.csv"  # blended ensemble output
 
 # iterate over models
 for x in "${ensemble[@]}"; do
   a=( ${x} )
   name="${a[0]}"
-  echo "$name"
   # iterate over splits
   for i in 1 2 3; do
     step="${a[$i]}"
-    echo "fold: ${i}; step: ${step}"
-    output_dir="$predictions_dir/fold${i}"
+    echo "model: $name; fold: $i; step: $step"
+    output_dir="$predictions_dir/fold$i"
     mkdir -p "$output_dir"
     python "$root_dir/jobs/predict.py" \
-      ++checkpoint_path="${models_dir}/fold${i}/${name}/checkpoint-${step}/pytorch_model.bin" \
-      ++output_path="${output_dir}/${name}__step${step}.npz" \
-      ++data_path=$data_path \
+      ++checkpoint_path="$models_dir/fold$i/$name/checkpoint-$step/pytorch_model.bin" \
+      ++output_path="$output_dir/${name}__step$step.npz" \
+      ++data_path="$data_path" \
       ++embeddings_dir=$embeddings_dir \
       ++output_format=npz
     echo "============================="
@@ -37,7 +36,7 @@ done
 
 # blend predictions
 python "$root_dir/jobs/blend.py" \
-  --data_path=${data_path} \
+  --data_path="$data_path" \
   --config_path="$ensemble_path" \
   --predictions_dir="$predictions_dir" \
-  --output_path=${blended_output_path}
+  --output_path="$blended_output_path"
